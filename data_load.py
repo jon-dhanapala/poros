9358032b25fd26abfe99aa3895fcda2627f84d60
ayser259 <ayserchoudhury@gmail.com>
2018-12-01 02:37:04 -0500

    Chi squared dist test

--- data_load.py
+++ data_load.py
@@ -1,12 +1,123 @@
 import pandas as pd
+import scipy.stats
+import numpy as np
 
+CDFS = [
+    "norm",            #Normal (Gaussian)
+    "alpha",           #Alpha
+    "anglit",          #Anglit
+    "arcsine",         #Arcsine
+    "beta",            #Beta
+    "betaprime",       #Beta Prime
+    "bradford",        #Bradford
+    "burr",            #Burr
+    "cauchy",          #Cauchy
+    "chi",             #Chi
+    "chi2",            #Chi-squared
+    "cosine",          #Cosine
+    "dgamma",          #Double Gamma
+    "dweibull",        #Double Weibull
+    "erlang",          #Erlang
+    "expon",           #Exponential
+    "exponweib",       #Exponentiated Weibull
+    "exponpow",        #Exponential Power
+    "fatiguelife",     #Fatigue Life (Birnbaum-Sanders)
+    "foldcauchy",      #Folded Cauchy
+    "f",               #F (Snecdor F)
+    "fisk",            #Fisk
+    "foldnorm",        #Folded Normal
+    "frechet_r",       #Frechet Right Sided, Extreme Value Type II
+    "frechet_l",       #Frechet Left Sided, Weibull_max
+    "gamma",           #Gamma
+    "gausshyper",      #Gauss Hypergeometric
+    "genexpon",        #Generalized Exponential
+    "genextreme",      #Generalized Extreme Value
+    "gengamma",        #Generalized gamma
+    "genlogistic",     #Generalized Logistic
+    "genpareto",       #Generalized Pareto
+    "genhalflogistic", #Generalized Half Logistic
+    "gilbrat",         #Gilbrat
+    "gompertz",        #Gompertz (Truncated Gumbel)
+    "gumbel_l",        #Left Sided Gumbel, etc.
+    "gumbel_r",        #Right Sided Gumbel
+    "halfcauchy",      #Half Cauchy
+    "halflogistic",    #Half Logistic
+    "halfnorm",        #Half Normal
+    "hypsecant",       #Hyperbolic Secant
+    "invgamma",        #Inverse Gamma
+    "invnorm",         #Inverse Normal
+    "invweibull",      #Inverse Weibull
+    "johnsonsb",       #Johnson SB
+    "johnsonsu",       #Johnson SU
+    "laplace",         #Laplace
+    "logistic",        #Logistic
+    "loggamma",        #Log-Gamma
+    "loglaplace",      #Log-Laplace (Log Double Exponential)
+    "lognorm",         #Log-Normal
+    "lomax",           #Lomax (Pareto of the second kind)
+    "maxwell",         #Maxwell
+    "mielke",          #Mielke's Beta-Kappa
+    "nakagami",        #Nakagami
+    "ncx2",            #Non-central chi-squared
+    "ncf",             #Non-central F
+    "nct",
+    "poisson"            #Non-central Student's T
+    "pareto",          #Pareto
+    "powerlaw",        #Power-function
+    "powerlognorm",    #Power log normal
+    "powernorm",       #Power normal
+    "rdist",           #R distribution
+    "reciprocal",      #Reciprocal
+    "rayleigh",        #Rayleigh
+    "rice",            #Rice
+    "recipinvgauss",   #Reciprocal Inverse Gaussian
+    "semicircular",    #Semicircular
+    "t",               #Student's T
+    "triang",          #Triangular
+    "truncexpon",      #Truncated Exponential
+    "truncnorm",       #Truncated Normal
+    "tukeylambda",     #Tukey-Lambda
+    "uniform",         #Uniform
+    "vonmises",        #Von-Mises (Circular)
+    "wald",            #Wald
+    "weibull_min",     #Minimum Weibull (see Frechet)
+    "weibull_max",     #Maximum Weibull (see Frechet)
+    "wrapcauchy",      #Wrapped Cauchy
+    "ksone",           #Kolmogorov-Smirnov one-sided (no stats)
+    "kstwobign"        #Kolmogorov-Smirnov two-sided test for Large N
+    ]
 
-def read_data(dir):
-    iat = pd.read_csv('interarrival_times.csv')
-    print(iat)
+def test_distribution(file_name,col_name):
+    sample = read_data(file_name)[col_name]
+    for cdf in CDFS:
+        try:
+            #fit our data set against every probability distribution
+            parameters = eval("scipy.stats."+cdf+".fit(sample)");
+            #Applying the Kolmogorov-Smirnof one sided test
+            D, p = scipy.stats.kstest(sample, cdf, args=parameters);
+            #pretty-print the results
+            print(cdf.ljust(16) + (",p: ,"+str(p)).ljust(25)+",D: ,"+str(D))
+        except:
+            c = 2
+
+def read_data(file_name):
+    iat = pd.read_csv(file_name)
+    return iat
+
+def chi_squared_test(file_name,col_name):
+    data = read_data(file_name)[col_name].sort_values().values
+    x = np.random.exponential((data.mean()),len(data)).round(decimals=2)
+    x = np.sort(x)
+    print(scipy.stats.chisquare(x,data))
+    # return exponential(1/lamda,read_data(file_name)[col_name])
 
 def main():
-    # Run When Script is run
+    # print("interarrival_times")
+    # test_distribution('interarrival_times.csv','inter_arrival_time')
+    # print("service_times")
+    # test_distribution('service_times.csv','service_times')
+    chi_squared_test('interarrival_times.csv','inter_arrival_time')
+    chi_squared_test('service_times.csv','service_times')
 
 if __name__ == '__main__':
     main()
